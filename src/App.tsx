070d754b3c6db088ae21ba57b612ec5ae7b7776c
lukachi <lukachisama@gmail.com>
2023-07-15 15:51:02 +0300

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,10 @@ const App: FC<HTMLAttributes<HTMLDivElement>> = ({ children }) => {
   const { provider, init: initWeb3 } = useWeb3Context()
 
   const init = useCallback(async () => {
+    if (provider?.address) return
+
     try {
-      if (!provider?.address) {
-        await initWeb3()
-      }
+      await initWeb3()
 
       document.title = config.APP_NAME
     } catch (error) {
@@ -44,20 +44,29 @@ const App: FC<HTMLAttributes<HTMLDivElement>> = ({ children }) => {
     const showErrorToast = (payload: unknown) => showToast('error', payload)
     const showInfoToast = (payload: unknown) => showToast('info', payload)
 
-    bus.on(BUS_EVENTS.success, showSuccessToast)
-    bus.on(BUS_EVENTS.warning, showWarningToast)
-    bus.on(BUS_EVENTS.error, showErrorToast)
-    bus.on(BUS_EVENTS.info, showInfoToast)
+    let mountingInit = async () => {
+      bus.on(BUS_EVENTS.success, showSuccessToast)
+      bus.on(BUS_EVENTS.warning, showWarningToast)
+      bus.on(BUS_EVENTS.error, showErrorToast)
+      bus.on(BUS_EVENTS.info, showInfoToast)
 
-    init()
+      await init()
+    }
+
+    mountingInit()
 
     return () => {
       bus.off(BUS_EVENTS.success, showSuccessToast)
       bus.off(BUS_EVENTS.warning, showWarningToast)
       bus.off(BUS_EVENTS.error, showErrorToast)
       bus.off(BUS_EVENTS.info, showInfoToast)
+
+      mountingInit = async () => {
+        /* empty */
+      }
     }
-  }, [init, showToast])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div id='app'>
