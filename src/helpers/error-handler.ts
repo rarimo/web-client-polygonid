38860362ca6fccc1bf0df212a081d27bac58e1eb
lukachi <lukachisama@gmail.com>
2023-07-17 17:41:10 +0300

    localize errors for conflicted address and empty state

--- src/helpers/error-handler.ts
+++ src/helpers/error-handler.ts
@@ -1,12 +1,19 @@
+import { RuntimeError } from '@distributedlab/tools'
+import { errors } from '@distributedlab/w3p'
 import log from 'loglevel'
 
 import { bus, BUS_EVENTS } from '@/helpers'
 import i18n from '@/localization'
 
+enum VERIFIER_INTERNAL_ERRORS {
+  conflictAddressesIdentity = `execution reverted: DemoVerifier: current address has already been used to verify another identity`,
+  emptyState = `execution reverted: QueryMTPValidatorOffChain: state doesn't exist in state contract`,
+}
+
 export class ErrorHandler {
   static process(error: Error | unknown, errorMessage = ''): void {
-    const msgTranslation = errorMessage || ErrorHandler._getErrorMessage(error)
-    bus.emit(BUS_EVENTS.error, msgTranslation)
+    const { msgTranslation, msgType } = ErrorHandler._getErrorMessage(error)
+    bus.emit(msgType as BUS_EVENTS, msgTranslation || errorMessage)
 
     ErrorHandler.processWithoutFeedback(error)
   }
@@ -15,17 +22,47 @@ export class ErrorHandler {
     log.error(error)
   }
 
-  static _getErrorMessage(error: Error | unknown): string {
+  static _getErrorMessage(error: Error | unknown): {
+    msgTranslation: string
+    msgType: 'error' | 'warning'
+  } {
     let errorMessage = ''
+    let msgType: 'error' | 'warning' = 'error'
 
     if (error instanceof Error) {
       switch (error.constructor) {
         default: {
-          errorMessage = i18n.t('errors.default')
+          if ('error' in error) {
+            const currentError = error.error as RuntimeError
+
+            switch (error?.error?.constructor) {
+              case errors.ProviderInternalError:
+                if (
+                  currentError?.message ===
+                  VERIFIER_INTERNAL_ERRORS.conflictAddressesIdentity
+                ) {
+                  errorMessage = i18n.t(
+                    'verifier-errors.conflict-addresses-identity',
+                  )
+                  msgType = 'warning'
+                } else if (
+                  currentError?.message === VERIFIER_INTERNAL_ERRORS.emptyState
+                ) {
+                  errorMessage = i18n.t('verifier-errors.empty-state')
+                  msgType = 'warning'
+                }
+                break
+              default:
+                errorMessage = i18n.t('errors.default')
+            }
+          }
         }
       }
     }
 
-    return errorMessage
+    return {
+      msgTranslation: errorMessage,
+      msgType: msgType || 'error',
+    }
   }
 }
