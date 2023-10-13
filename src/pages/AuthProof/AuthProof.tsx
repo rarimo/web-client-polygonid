cb5f1d212bd988ba586645cb61e6b3c3f1ade8b9
lukachi <lukachisama@gmail.com>
2023-10-13 13:30:47 +0300

    update markup

--- src/pages/AuthProof/AuthProof.tsx
+++ src/pages/AuthProof/AuthProof.tsx
@@ -18,8 +18,17 @@ const AuthProof: FC<Props> = () => {
 
   return (
     <div className='auth-proof'>
-      {isPending ? (
-        <Loader className='auth-proof__loader' />
+      {!isPending ? (
+        <>
+          <div className='auth-proof__header'>
+            <h2 className='auth-proof__header-title'>{`Check Transition states`}</h2>
+            <span className='auth-proof__header-subtitle'>{`Please wait...`}</span>
+          </div>
+
+          <div className='auth-proof__loader-wrp'>
+            <Loader className='auth-proof__loader' />
+          </div>
+        </>
       ) : (
         <>
           <div className='auth-proof__header'>
